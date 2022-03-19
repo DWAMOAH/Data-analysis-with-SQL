06bc796575d6c81ffcda8117ef9f7e3f41c2d4cd
Dwamena Amoah <99998198+DWAMOAH@users.noreply.github.com>
2022-03-19 10:46:29 +0000

    update with new statement
    added statement on count based condition

--- SQL_case_statement.sql
+++ SQL_case_statement.sql
@@ -8,3 +8,15 @@ then labstart.customer_age
 else null
 end as age_analyze
 from labstart
+
+---count based on condition
+---this is to find if customer's year meet a specific condition
+---ie if the year is greater than 2015 then get the age if not give nothing
+select
+labstart.year,
+case when labstart.year>2011
+then count(labstart.customer_age)
+else null
+end as number_of_customers
+from labstart
+group by labstart.year
